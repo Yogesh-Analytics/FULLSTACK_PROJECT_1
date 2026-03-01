072dc737eb520ab3c5872970a2f172bcce12b279
Yogi <yogeshfulari2004@gmail.com>
2026-03-01 13:43:35 +0530

    Use TLS 587 for Nodemailer

--- backend/server.js
+++ backend/server.js
@@ -122,15 +122,15 @@ app.post("/contact", async (req, res) => {
   const { name, email, phone, message } = req.body;
 
   try {
-    const transporter = nodemailer.createTransport({
-     host: "smtp.gmail.com",
-     port: 587,       
-     secure: false,   
-     auth: {
+   const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,       
+    secure: false,   
+    auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
-    },
-  });
+  },
+});
 
     await transporter.sendMail({
       from: process.env.EMAIL_USER,
