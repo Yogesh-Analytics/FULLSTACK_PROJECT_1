3c4c46be60e7bc235606cdbdd93a8638f6b88f1d
Yogi <yogeshfulari2004@gmail.com>
2026-03-01 13:31:07 +0530

    Update Nodemailer transporter to TLS for Render

--- backend/server.js
+++ backend/server.js
@@ -123,12 +123,14 @@ app.post("/contact", async (req, res) => {
 
   try {
     const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-    });
+     host: "smtp.gmail.com",
+     port: 587,       
+     secure: false,   
+     auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
 
     await transporter.sendMail({
       from: process.env.EMAIL_USER,
